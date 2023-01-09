2c345a7947be35c2773cc73c9890804365fd58fa
Armangrig <118439516+Armangrig@users.noreply.github.com>
2023-01-10 00:11:48 +0400

    Update main.tf

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -18,7 +18,7 @@ provider "aws" {
   region  = "us-east-2"
 }
 resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.ubuntu.7fa392ce-1d09-4fd2-aea1-3cb721e25f47
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
   key_name      = "app-ssh-key"
 
