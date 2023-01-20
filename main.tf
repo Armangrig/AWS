e44c7a016c15c60ddc5a569e9c745f0ab170ca30
Armangrig <118439516+Armangrig@users.noreply.github.com>
2023-01-20 20:35:58 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,13 +10,13 @@ terraform {
   }
   
   
-terraform {
-  backend "s3" {
-    bucket = "my-terraform-state"
-    key    = "state.tfstate"
-    region = "us-east-1"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = "my-terraform-state"
+#    key    = "state.tfstate"
+#    region = "us-east-1"
+#  }
+#}
   
 #  resource "aws_s3_bucket" "terraform_state" {
 #  bucket = "lennakan"
